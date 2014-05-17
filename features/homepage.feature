4100a53f5ccaaaf6fe1738a051f535c0d2d61a94
Louise <louiselai88@gmail.com>
2014-05-17 10:50:49 +0100

    added views and index.erb file

--- features/homepage.feature
+++ features/homepage.feature
@@ -4,4 +4,7 @@ Scenario: visiting the homepage
 	When I visit the homepage
 	Then I should find a greeting 
 
+Scenario: how the homepage is set up
+	When I visit the homepage
+	Then I should see boxes for me to add links
 	
---
+++ features/step_definitions/homepage_steps.rb
@@ -0,0 +1,12 @@
+When(/^I visit the homepage$/) do
+  visit '/'
+end
+
+Then(/^I should find a greeting$/) do
+	greeting = "Hello BookmarkManager!"
+	expect(page).to have_content(greeting)
+end
+
+Then(/^I should see boxes for me to add links$/) do
+	find_field('URL') && find_field('Name')
+end
