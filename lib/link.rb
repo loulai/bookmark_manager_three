0df620aa86a2862b5bb44e3b2094bc735f21d71e
Louise <louiselai88@gmail.com>
2014-05-18 16:22:57 +0100

    fixed gem name typo

--- lib/link.rb
+++ lib/link.rb
@@ -1,8 +1,13 @@
 require 'data_mapper'
+require 'dm-postgres-adapter'
 
 class Link
 
 	include DataMapper::Resource
+
+		property :id, Serial
+		property :url, Text
+		property :name, Text
 
 
 end
